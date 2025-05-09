d4159b2f4e844c7d0814158a9f0171e5b5df6afb
Neeraj Kumar <neerajkumarkondabattula@gmail.com>
2025-05-09 19:10:10 +0530

    Updated Routine

--- src/components/sections/Routine.jsx
+++ src/components/sections/Routine.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const Routine = () => {
   return (
-    <section className="w-full h-[580px] max-w-[1200px] lg:flex h-full mx-auto bg-black lg:bg-[#BDBDBD] lg:flex-row-reverse">
-      <div className="w-full lg:w-[50%] overflow-hidden">
+    <section className="w-full h-[auto] lg:h-[580px] max-w-[1200px] lg:flex mx-auto lg:flex-row-reverse lg:bg-[url(https://cdn.shopify.com/s/files/1/0637/6194/0660/files/Group_1000007430.png?v=1746796173)] bg-cover bg-center">
+      <div className="w-full lg:w-[50%] overflow-hidden lg:hidden">
         <img
           className=""
           src="https://cdn.shopify.com/s/files/1/0637/6194/0660/files/image_319.png?v=1746792738"
           alt=""
         />
       </div>
-      <div className="w-full lg:w-[50%] p-10 md:p-15 md:mx-auto lg:py-10 text-center text-black my-auto bg-transparent absolute top-20 lg:static">
+      <div className="w-full lg:w-[50%] p-10 md:p-15 md:mx-auto lg:py-10 text-center text-white my-auto  absolute top-20 md:top-80 lg:top-36 lg:left-20 lg:float-left bg-trasparent">
         <div className="flex flex-col gap-3">
-          <p className="text-2xl md:text-xl font-md mb-[-10px]">Skincare</p>
+          <p className="text-3xl md:text-4xl font-md mb-[-10px]">Skincare</p>
           <h1 className="text-4xl lg:text-5xl w-[100%] text-center font-bold leading-14 tracking-tight ">
             Routine Finder
           </h1>
@@ -25,9 +25,7 @@ const Routine = () => {
             reach your grooming goals faster.
           </p>
           <button className="bg-[#00048a] w-[90%] md:w-[80%] lg:w[70%] py-2 px-4 mx-auto font-semibold hover:scale-[1.04] duration-100 ease-in-out">
-            <a href="https://www.reginaldmen.com/?utm_source=Quiz&utm_medium=home_direct&utm_campaign=SkinTest#quiz-gWH2J3">
-              Take The Quiz
-            </a>
+            <a href="">Take The Quiz</a>
           </button>
         </div>
       </div>
